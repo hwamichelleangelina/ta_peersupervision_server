bc1bfcaff4615c71e1967c038b89b265a69d85d4
18320007 Michelle Angelina <hwamichelleangelina@gmail.com>
2024-06-05 21:39:51 +0700

    Progress Deployment 05-06-2024 21:40

--- config/db.js
+++ config/db.js
@@ -10,73 +10,10 @@ const mysqlConn = mysql.createConnection({
 
 mysqlConn.connect(function (error) {
     if (error) {
-        console.error('Error connecting to database:', error.message);
+        console.error('Error connecting to database:', err.message);
     } else {
         console.log('Connected to MySQL database: peersupervision.');
     }
-
-  // Jalankan perintah ALTER TABLE
-  const query = `ALTER TABLE \`peersupervision\`.\`psusers\` 
-                 CHANGE COLUMN \`psuadded\` \`psuadded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
-
-  connection.query(query, (error, results, fields) => {
-    if (error) {
-      console.error('An error occurred while executing the query');
-      return;
-    }
-    console.log('Table altered successfully:', results);
-  });
-
-  // Jalankan perintah ALTER TABLE
-  const bkuserquery = `ALTER TABLE \`peersupervision\`.\`bkusers\` 
-                 CHANGE COLUMN \`bkuadded\` \`bkuadded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
-
-  connection.query(bkuserquery, (error, results, fields) => {
-    if (error) {
-      console.error('An error occurred while executing the query');
-      return;
-    }
-    console.log('Table altered successfully:', results);
-  });
-
-  // Jalankan perintah ALTER TABLE
-  const laporanquery = `ALTER TABLE \`peersupervision\`.\`laporan\` 
-                 CHANGE COLUMN \`laporanadded\` \`laporanadded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
-
-  connection.query(laporanquery, (error, results, fields) => {
-    if (error) {
-      console.error('An error occurred while executing the query');
-      return;
-    }
-    console.log('Table altered successfully:', results);
-  });
-
-  // Jalankan perintah ALTER TABLE
-  const jadwalquery = `ALTER TABLE \`peersupervision\`.\`jadwal\` 
-                 CHANGE COLUMN \`jadwaladded\` \`jadwaladded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
-
-  connection.query(jadwalquery, (error, results, fields) => {
-    if (error) {
-      console.error('An error occurred while executing the query');
-      return;
-    }
-    console.log('Table altered successfully:', results);
-  });
-
-  // Jalankan perintah ALTER TABLE
-  const dampinganquery = `ALTER TABLE \`peersupervision\`.\`dampingan\` 
-                 CHANGE COLUMN \`dampinganadded\` \`dampinganadded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
-
-  connection.query(dampinganquery, (error, results, fields) => {
-    if (error) {
-      console.error('An error occurred while executing the query');
-      return;
-    }
-    console.log('Table altered successfully:', results);
-  });
-
-  // Tutup koneksi
-  connection.end();
 });
 
 module.exports = mysqlConn;
