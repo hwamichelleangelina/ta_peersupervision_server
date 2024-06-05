fd1ff1732ff946eea79b8bd326f563956eab4600
18320007 Michelle Angelina <hwamichelleangelina@gmail.com>
2024-06-05 21:33:57 +0700

    Progress Deployment For Altering Table Temporary

--- config/db.js
+++ config/db.js
@@ -14,6 +14,69 @@ mysqlConn.connect(function (error) {
     } else {
         console.log('Connected to MySQL database: peersupervision.');
     }
+
+  // Jalankan perintah ALTER TABLE
+  const query = `ALTER TABLE \`peersupervision\`.\`psusers\` 
+                 CHANGE COLUMN \`psuadded\` \`psuadded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
+
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error('An error occurred while executing the query:', error.stack);
+      return;
+    }
+    console.log('Table altered successfully:', results);
+  });
+
+  // Jalankan perintah ALTER TABLE
+  const bkuserquery = `ALTER TABLE \`peersupervision\`.\`bkusers\` 
+                 CHANGE COLUMN \`bkuadded\` \`bkuadded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
+
+  connection.query(bkuserquery, (error, results, fields) => {
+    if (error) {
+      console.error('An error occurred while executing the query:', error.stack);
+      return;
+    }
+    console.log('Table altered successfully:', results);
+  });
+
+  // Jalankan perintah ALTER TABLE
+  const laporanquery = `ALTER TABLE \`peersupervision\`.\`laporan\` 
+                 CHANGE COLUMN \`laporanadded\` \`laporanadded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
+
+  connection.query(laporanquery, (error, results, fields) => {
+    if (error) {
+      console.error('An error occurred while executing the query:', error.stack);
+      return;
+    }
+    console.log('Table altered successfully:', results);
+  });
+
+  // Jalankan perintah ALTER TABLE
+  const jadwalquery = `ALTER TABLE \`peersupervision\`.\`jadwal\` 
+                 CHANGE COLUMN \`jadwaladded\` \`jadwaladded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
+
+  connection.query(jadwalquery, (error, results, fields) => {
+    if (error) {
+      console.error('An error occurred while executing the query:', error.stack);
+      return;
+    }
+    console.log('Table altered successfully:', results);
+  });
+
+  // Jalankan perintah ALTER TABLE
+  const dampinganquery = `ALTER TABLE \`peersupervision\`.\`dampingan\` 
+                 CHANGE COLUMN \`dampinganadded\` \`dampinganadded\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;`;
+
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error('An error occurred while executing the query:', error.stack);
+      return;
+    }
+    console.log('Table altered successfully:', results);
+  });
+
+  // Tutup koneksi
+  connection.end();
 });
 
 module.exports = mysqlConn;
