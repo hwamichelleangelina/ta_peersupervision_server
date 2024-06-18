96f4f00349cddb89858f5b44ea31f5a6229254f9
18320007 Michelle Angelina <hwamichelleangelina@gmail.com>
2024-06-18 22:45:38 +0700

    Deployment Progress 18-06-2024 22:45

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 function createMySQLConnection() {
     const connection = mysql.createConnection({
         host: 'localhost',
-        user: 'root',
+        user: 'peersupervision',
         password: 'supervise',
         database: 'peersupervision',
     });
